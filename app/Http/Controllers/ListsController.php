ae7351264180aae9daf087bb5415af7011632130
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-15 22:22:37 +0200

    refactor lists controller into OG lists controller

--- app/Http/Controllers/ListsController.php
+++ app/Http/Controllers/ListsController.php
@@ -2,41 +2,9 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\Lists;
-use App\Models\Task;
 use Illuminate\Http\Request;
 
 class ListsController extends Controller
 {
-    public static function index()
-    {
-        $lists = Lists::all();
-        return view('sections.lists', [
-            'lists' => $lists]
-        );
-    }
-
-    public static function create(Request $request)
-    {
-        $validated = $request->validate([
-            'name' => ['required','string', 'max:255']
-        ]);
-
-        $list = new Lists;
-        $list->name = $validated['name'];
-        $list->data = "";
-
-        $list->save();
-        return redirect('/lists');
-    }
-
-    public static function show($id)
-    {
-        $tasks = Task::where('list', $id)->get();
-
-        return view('sections.list', [
-            'id'    => $id,
-            'tasks' => $tasks
-        ]);
-    }
+    //
 }
--- routes/web.php
+++ routes/web.php
@@ -4,8 +4,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ListsController;
-use App\Http\Controllers\TasksController;
+use App\Http\Controllers\ListsOgController;
 use App\Http\Controllers\ListsXhrController;
+use App\Http\Controllers\TasksController;
 use App\Http\Controllers\TasksXhrController;
 
 /*
@@ -28,14 +29,17 @@ Route::middleware('auth')->group(function() {
 
     // Basic "OG" routes that require user to be authenticated
     Route::prefix('og')->group(function () {
-        Route::get('lists', [ListsController::class, 'index']);
-        Route::post('lists', [ListsController::class, 'create']);
-        Route::get('list/{id}', [ListsController::class, 'show']);
+        Route::get('lists', [ListsOgController::class, 'index']);
+        Route::post('lists', [ListsOgController::class, 'create']);
+        Route::get('list/{id}', [ListsOgController::class, 'show']);
     
         Route::get('tasks', [TasksController::class, 'index']);
         Route::post('task', [TasksController::class, 'create']);
         Route::post('task/{id}/edit', [TasksController::class, 'update']);
     });
+
+    // VUE.js app
+    Route::get('list/{id}', [ListsController::class, 'show']);
 });
 
 Route::get('/', function () {
