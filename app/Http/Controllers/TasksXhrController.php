31466f129b331968da3a50b0eae10c606e366de6
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-16 18:40:28 +0000

    add controller to handle task updates via xhr

--- app/Http/Controllers/TasksXhrController.php
+++ app/Http/Controllers/TasksXhrController.php
@@ -24,7 +24,22 @@ class TasksXhrController extends Controller
         $task->complete = 0;
         $task->save();
         return response()->json([
-            'created' => 'true',
+            'task' => $task
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $validated = $request->validate([
+            'title' => ['nullable','string','max:255'],
+            'list'  => ['nullable','integer','min:0','max:99999'],
+            'complete' => ['nullable','integer','min:0','max:1']
+        ]);
+
+        $task = Task::find($id);
+        $task->fill($validated);
+        $task->save();
+        return response()->json([
             'task' => $task
         ]);
     }
