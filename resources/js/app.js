d2821e421f33a707bf1944bf99bd23c4073d0ffa
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-20 18:39:18 +0000

    bug: only mount app if todo id is found

--- resources/js/app.js
+++ resources/js/app.js
@@ -15,10 +15,13 @@ import BaseCard from './components/BaseCard.vue';
 import BaseButton from './components/BaseButton.vue';
 import TabsWrapper from './components/TabsWrapper.vue';
 
-const app = createApp(App);
-
-app.component('base-card', BaseCard);
-app.component('base-button', BaseButton);
-app.component('tabs-wrapper', TabsWrapper);
-
-app.mount('#todo');
+const todoList = document.getElementById('todo');
+if(todoList){
+    const app = createApp(App);
+    
+    app.component('base-card', BaseCard);
+    app.component('base-button', BaseButton);
+    app.component('tabs-wrapper', TabsWrapper);
+    
+    app.mount('#todo');
+}
