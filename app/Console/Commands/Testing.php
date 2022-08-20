29918f6d511a36327209d2e780d7aa38e42b4614
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-20 10:08:27 +0000

    add console command to test WS communication

---
+++ app/Console/Commands/Testing.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Events\TaskUpdated;
+use Illuminate\Console\Command;
+
+class Testing extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'todo:test';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        broadcast(new TaskUpdated(2, 2, 1));
+    }
+}
