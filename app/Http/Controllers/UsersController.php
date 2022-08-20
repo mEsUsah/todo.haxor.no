deeb6b44c1554fe250b8c095c2a3f93c10afa7d6
Stanley Skarshaug <stanley.skarshaug@gmail.com>
2022-08-20 12:15:29 +0000

    add UsersController with index and create

---
+++ app/Http/Controllers/UsersController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UsersController extends Controller
+{
+    public static function index()
+    {
+        $users = User::all();
+        return view('sections.users.index', [
+            'users' => $users]
+        );
+    }
+
+    protected function create(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:8', 'confirmed'],
+        ]);
+
+        $user = new User;
+        $user->name = $validated['name'];
+        $user->email = $validated['email'];
+        $user->password = Hash::make($validated['password']);
+        $user->save();
+        return redirect('/users');
+    }
+}
