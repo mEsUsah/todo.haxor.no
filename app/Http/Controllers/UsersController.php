aa9a8d379069c5eb883ff3f6b1dd8ca95179340c
Stanley Skarshaug <stanley@haxor.no>
2024-10-01 23:22:42 +0200

    fix(UserController): implement UserPolicy

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
@@ -11,18 +12,16 @@ class UsersController extends Controller
 {
     public static function index()
     {
-        if(Auth::user()->privilege_id == 2) {
-            $users = User::all();
-            return view('sections.users.index', [
-                'users' => $users]
-            );
-        } else {
-            return redirect()->route('lists');
-        }
+        Gate::authorize('viewAny', User::class);
+        $users = User::all();
+        return view('sections.users.index', [
+            'users' => $users]
+        );
     }
 
     protected function create(Request $request)
     {
+        Gate::authorize('create', User::class);
         $validated = $request->validate([
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
